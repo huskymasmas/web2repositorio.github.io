5e7010e2f634e45815f1d97d4b6eb440cdda3df9
huskymasmas <rcuettolusky@gmail.com>
2024-10-31 11:09:21 -0600

    iestructura

---
+++ js/verproducto.js
@@ -0,0 +1,16 @@
+
+
+    var nombre  = document.getElementById("nombre");
+
+
+
+
+    function producto( nombredeproducto ){
+
+    document.write(nombredeproducto);
+
+
+
+    }
+
+    nombre.addEventListener('click',producto,true);
